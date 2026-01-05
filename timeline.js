330ae87f430e5e89e61332422526e2c64cb42a4b
azoz20121234-ui <azoz20121234@gmail.com>
2026-01-05 19:06:53 +0300

    Add addEvent function to manage timeline events

--- timeline.js
+++ timeline.js
@@ -1,1 +1,9 @@
+import { state } from "./state.js";
 
+export function addEvent(title, meta={}){
+  state.timeline.unshift({
+    time: new Date().toLocaleTimeString("ar-SA"),
+    title,
+    meta
+  });
+}
