82695bd07b6596d6be9bb829981654ec88321a7f
azoz20121234-ui <azoz20121234@gmail.com>
2026-01-05 19:13:31 +0300

    Refactor addEvent function to remove meta parameter

--- timeline.js
+++ timeline.js
@@ -1,9 +1,4 @@
 import { state } from "./state.js";
-
-export function addEvent(title, meta={}){
-  state.timeline.unshift({
-    time: new Date().toLocaleTimeString("ar-SA"),
-    title,
-    meta
-  });
+export function addEvent(title){
+  state.timeline.unshift({time:new Date().toLocaleTimeString("ar-SA"),title});
 }
